cd81c8b9930b5b71848cb7243c7562ece9502f9e
James <Icedshot@gmail.com>
2018-01-28 08:53:45 +0000

    fun time

--- main.cpp
+++ main.cpp
@@ -1,9 +1,69 @@
 #include <iostream>
-
-using namespace std;
+#include <cl/cl.h>
+#include <ocl/ocl.hpp>
+#include <ocl/logging.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    lg::set_logfile("./out.txt");
+    lg::redirect_to_stdout();
+
+    lg::log("Test");
+
+    sf::RenderWindow win;
+    win.create(sf::VideoMode(800, 600), "Test");
+
+    cl::context ctx;
+
+    cl::program program(ctx, "fluid.cl");
+    program.build_with(ctx, "");
+
+    //cl::kernel test_kernel(program, "test_kernel");
+
+    cl::command_queue cqueue(ctx);
+
+    cl::buffer_manager buffer_manage;
+
+    cl::buffer* buf = buffer_manage.fetch<cl::buffer>(ctx, nullptr);
+
+    std::vector<int> data;
+
+    for(int i=0; i < 800*600; i++)
+    {
+        data.push_back(i);
+    }
+
+    buf->alloc(cqueue, data);
+
+    cl::cl_gl_interop_texture* interop = buffer_manage.fetch<cl::cl_gl_interop_texture>(ctx, nullptr, win.getSize().x, win.getSize().y);
+    interop->acquire(cqueue);
+
+    cl::args none;
+    none.push_back(buf);
+    none.push_back(interop);
+
+    cqueue.exec(program, "fluid_test", none, {128}, {16});
+
+    cqueue.block();
+
+    while(win.isOpen())
+    {
+        sf::Event event;
+
+        while(win.pollEvent(event))
+        {
+
+        }
+
+        cqueue.exec(program, "fluid_test", none, {800, 600}, {16, 16});
+        cqueue.block();
+
+        interop->gl_blit_me(0, cqueue);
+
+        win.display();
+        win.clear();
+    }
+
     return 0;
 }
