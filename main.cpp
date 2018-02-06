9e250d2b58d90f9a239d3aeaccc11b1442528084
James <Icedshot@gmail.com>
2018-02-06 04:56:07 +0000

    interesting but low performance rendertexture solution, too much interop required

--- main.cpp
+++ main.cpp
@@ -25,6 +25,8 @@ int main()
     sf::RenderWindow win;
     win.create(sf::VideoMode(window_size.x(), window_size.y()), "Test", sf::Style::Default, settings);
 
+    #if 1
+
     b3OpenCLUtils_clewInit();
 
     cl::context ctx;
@@ -43,7 +45,8 @@ int main()
     cl::buffer_manager buffer_manage;
 
 
-    cl::cl_gl_interop_texture* interop = buffer_manage.fetch<cl::cl_gl_interop_texture>(ctx, nullptr, win.getSize().x, win.getSize().y);
+    cl::cl_gl_interop_texture* interop = buffer_manage.fetch<cl::cl_gl_interop_texture>(ctx, nullptr);
+    interop->create_renderbuffer(win.getSize().x, win.getSize().y);
     interop->acquire(cqueue);
 
 
@@ -73,7 +76,8 @@ int main()
     intermediate_tex.create(10, 10);
 
     sf::CircleShape shape;
-    shape.setRadius(5.f);
+    shape.setRadius(500.f);
+    shape.setFillColor(sf::Color(255,255,255,255));
 
     shape.setPosition(5, 5);
     shape.setOrigin(5, 5);
@@ -85,9 +89,22 @@ int main()
     const sf::Texture& ctex = intermediate_tex.getTexture();
     unsigned int glid = ctex.getNativeHandle();
 
-    cl::cl_gl_interop_texture* circletex = buffer_manage.fetch<cl::cl_gl_interop_texture>(ctx, nullptr, (GLuint)glid);
+    cl::cl_gl_interop_texture* circletex = buffer_manage.fetch<cl::cl_gl_interop_texture>(ctx, nullptr);
+    circletex->create_from_texture(glid);
     circletex->acquire(cqueue);
+    #endif // 0
     ///END HACKY CIRCLE TEXTURE STUFF
+
+    sf::RenderTexture render_tex;
+    render_tex.create(win.getSize().x, win.getSize().y);
+
+    render_tex.draw(shape);
+    render_tex.clear();
+    render_tex.display();
+
+    cl::cl_gl_interop_texture* test_screen = buffer_manage.fetch<cl::cl_gl_interop_texture>(ctx, nullptr);
+    test_screen->create_from_texture(render_tex.getTexture().getNativeHandle());
+    test_screen->acquire(cqueue);
 
     sf::Clock clk;
     sf::Keyboard key;
@@ -98,6 +115,10 @@ int main()
     vec2f cur_mouse = {0,0};
 
     bool running = true;
+
+    win.resetGLStates();
+    PFNGLBINDFRAMEBUFFEREXTPROC glBindFramebufferEXT = (PFNGLBINDFRAMEBUFFEREXTPROC)wglGetProcAddress("glBindFramebufferEXT");
+    glBindFramebufferEXT(GL_DRAW_FRAMEBUFFER, 0);
 
     while(running)
     {
@@ -116,6 +137,7 @@ int main()
 
         vec2f diff = cur_mouse - last_mouse;
 
+        #if 1
         if(mouse.isButtonPressed(sf::Mouse::Left))
         {
             fluid_manage.apply_force( cqueue, 0.1f, cur_mouse, diff);
@@ -140,51 +162,83 @@ int main()
                 }
             }
         }
+        #endif // 0
 
         while(win.pollEvent(event))
         {
             if(event.type == sf::Event::Closed)
                 running = false;
         }
+        #if 1
 
         if(use_cpu_physics)
             physics.issue_gpu_reads(cqueue, fluid_manage.get_velocity_buf(0), fluid_manage.physics_tex[fluid_manage.which_physics_tex], fluid_manage.velocity_to_display_ratio);
 
-        fluid_manage.tick(interop, buffer_manage, cqueue);
-        fluid_manage.render_fluid(interop, cqueue);
+        test_screen->acquire(cqueue);
+
+        cl::args cargs;
+        cargs.push_back(test_screen);
+        cqueue.exec("clear_image", cargs, {test_screen->w, test_screen->h}, {16, 16});
+
+        fluid_manage.tick(test_screen, buffer_manage, cqueue);
+        fluid_manage.render_fluid(test_screen, cqueue);
 
         ///for some reason nothing shows up if we render after ticking
         ///dont understand why
 
         if(!use_cpu_physics)
         {
-            physics_gpu.render(cqueue, interop, circletex);
+            physics_gpu.render(cqueue, test_screen, circletex);
             physics_gpu.tick(elapsed_s, fluid_manage.timestep_s, fluid_manage.get_velocity_buf(0), phys_queue);
         }
 
-        //lighting_manage.tick(interop, buffer_manage, cqueue, cur_mouse, fluid_manage.dye[fluid_manage.which_dye]);
+        //lighting_manage.tick(test_screen, buffer_manage, cqueue, cur_mouse, fluid_manage.dye[fluid_manage.which_dye]);
 
         if(use_cpu_physics)
         {
             physics.tick(elapsed_s, fluid_manage.timestep_s);
         }
 
-        fluid_manage.render_sand(interop, cqueue);
+        //fluid_manage.render_sand(test_screen, cqueue);
 
-        interop->gl_blit_me(0, cqueue);
+        //test_screen->gl_blit_me(0, cqueue);
 
+        cqueue.block();
+        test_screen->unacquire(cqueue);
+        #endif // 0
+
+        cqueue.block();
+        render_tex.display();
+
+        ///has to be drawn on top annoyingly
+        ///need to figure out a way to composite from the cpu, or
+        #if 1
         if(use_cpu_physics)
         {
-            physics.render(win);
+            physics.render(render_tex);
         }
+        #endif // 0
+
+        render_tex.display();
+
+        fluid_manage.render_sand(test_screen, cqueue);
 
         if(key.isKeyPressed(sf::Keyboard::Escape))
             system("Pause");
 
+        test_screen->unacquire(cqueue);
+
+        cqueue.block();
+        render_tex.display();
+
+        win.draw(sf::Sprite(render_tex.getTexture()));
+
         win.display();
         win.clear();
 
+        #if 1
         cqueue.block();
+        //render_tex.clear();
         ///TODO:
         ///should do one frame ahead shenanigans
 
@@ -201,6 +255,7 @@ int main()
         {
             physics.process_gpu_reads();
         }
+        #endif // 0
     }
 
     return 0;
